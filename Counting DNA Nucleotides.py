5dddd9baaf7903e32e5474dc18420b3aff4e4a63
Dorota Karkut <dorakarkut1@wp.pl>
2021-05-27 18:17:08 +0200

    changed to function

--- Counting DNA Nucleotides.py
+++ Counting DNA Nucleotides.py
@@ -1,15 +1,20 @@
-with open("rosalind_dna.txt", 'r') as file:
-	string = file.read()
+def counting_dna_nucleotides(file_path):
+	with open(file_path, 'r') as file:
+		string = file.read()
 
 
-nuc_dict = {
-	'A' : 0,
-	'C' : 0,
-	'G' : 0,
-	'T' : 0
-	}
+	nuc_dict = {
+		'A' : 0,
+		'C' : 0,
+		'G' : 0,
+		'T' : 0
+		}
 
-for nuc in nuc_dict:
-	nuc_dict[nuc] = string.count(nuc)
+	for nuc in nuc_dict:
+		nuc_dict[nuc] = string.count(nuc)
 
-print(nuc_dict)
+	return nuc_dict
+
+if __name__ == '__main__':
+	file_path = "rosalind_dna.txt"
+	print(counting_dna_nucleotides(file_path))
