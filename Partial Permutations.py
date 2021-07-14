9ba555e893a47300da0135f6a47ff7f13a0e1b99
Dorota Karkut <dorakarkut1@wp.pl>
2021-07-14 10:18:55 +0200

    Add Partial Permutations.py

---
+++ Partial Permutations.py
@@ -0,0 +1,11 @@
+﻿"""
+Given: Positive integers n and k such that 100≥n>0 and 10≥k>0.
+
+Return: The total number of partial permutations P(n,k), modulo 1,000,000.
+"""
+
+from math import factorial
+n = 100
+k = 9
+print((factorial(n)/factorial(n-k))%1000000)
+
